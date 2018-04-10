048ef72faa9bff90f5658823ff597520d56e3918
sxx <425033441@qq.com>
2018-04-10 22:23:16 +0800

    报错了我的天

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,13 +1,37 @@
 <template>
 <div>
   <p>登陆组件</p>
+  <form @submit.prevent="login">
+    <div>
+      <label for="username">用户名</label>
+      <input
+        type="text"
+        id="username"
+        v-model="userForm.username">
+    </div>
+    <div>
+      <label for="password">密码</label>
+      <input
+        type="password"
+        id="password"
+        v-model="userForm.password">
+    </div>
+    <div>
+      <button>点击登陆</button>
+    </div>
+  </form>
 </div>
 </template>
 
 <script>
 export default {
   data () {
-    return {}
+    return {
+      userForm: {
+        username: '',
+        password: ''
+      }
+    }
   }
 }
 </script>
